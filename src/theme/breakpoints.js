539c0ad14bbcf77b8772cca1c6553f74b1808cc7
Christian Keller <k.christian9@web.de>
2023-08-29 15:51:35 +0200

    init

---
+++ src/theme/breakpoints.js
@@ -0,0 +1,13 @@
+export const size = {
+    mobile: 411,
+    tablet: 768,
+    laptop: 1400,
+    desktop: 1921
+}
+
+export const device = {
+    mobile: `(max-width: ${size.mobile}px)`,
+    tablet: `(max-width: ${size.tablet}px)`,
+    laptop: `(max-width: ${size.laptop}px)`,
+    desktopXL: `(min-width: ${size.desktop}px)`
+};
---
+++ src/theme/global.js
@@ -0,0 +1,23 @@
+import { createGlobalStyle } from "styled-components";
+import colors from "./colors";
+import spacing from "./spacing";
+import font from "./font";
+import variables from "./variables";
+
+const GlobalStyle = createGlobalStyle`
+    *{
+        margin: 0;
+        padding: 0;
+        border: none;
+        line-height: 1;
+        box-sizing: border-box;
+    }
+    
+    ${colors}
+    ${spacing}
+    ${font}
+    ${variables}
+    
+`;
+
+export default GlobalStyle;
