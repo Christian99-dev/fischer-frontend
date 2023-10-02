02e516abeedef9f9d8d4f0e4a77cf483623baa40
Christian Keller <k.christian9@web.de>
2023-10-02 13:10:32 +0200

    backend + env

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,11 @@
 /**
  * @type {import('gatsby').GatsbyConfig}
  */
+
+require("dotenv").config({
+  path: `.env.${process.env.NODE_ENV}`,
+})
+
 module.exports = {
   siteMetadata: {
     title: `Zimmerei Fischer`,
@@ -8,5 +13,15 @@ module.exports = {
     description:
       "Willkommen bei [Ihr Firmenname] - Ihre Experten für individuelle Zimmerarbeiten und Holzbau in [Ihrem Standort]. Unser erfahrenes Team aus Zimmerleuten und Holzhandwerkern steht Ihnen zur Verfügung, um Ihre Wohnträume wahr werden zu lassen. Von maßgeschneiderten Holzmöbeln über Dachstühle bis hin zu Innenausbauten bieten wir hochwertige Handwerkskunst und kreative Lösungen. Entdecken Sie unser Portfolio und kontaktieren Sie uns noch heute für eine unverbindliche Beratung. Vertrauen Sie auf unsere jahrzehntelange Erfahrung und Leidenschaft für Holz - Ihre Zimmerei in [Ihrem Standort].",
   },
-  plugins: ["gatsby-plugin-styled-components"],
+  plugins: [
+    "gatsby-plugin-styled-components",
+    {
+      resolve: "gatsby-source-strapi",
+      options: {
+        apiURL: process.env.GATSBY_BACKEND_URL,
+        singleTypes : ["willkommen"],
+        accessToken:process.env.GATSBY_BACKEND_API_TOKEN
+      },
+    },
+  ],
 };
