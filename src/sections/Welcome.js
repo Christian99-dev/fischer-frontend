0acdec0701cd973c584feea807df9965275f5c44
Christian Keller <k.christian9@web.de>
2023-10-31 16:22:03 +0100

    test

--- src/sections/Welcome.js
+++ src/sections/Welcome.js
@@ -176,7 +176,7 @@ const WelcomeStyle = styled.section`
       height: 100%;
       width: 100%;
       z-index: 32;
-      background-color: green;
+      background-color: var(--black);
       opacity: 0.4;
     }
 
